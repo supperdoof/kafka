5435cdf66d4ac3280255050d6d3a913fb928a38a
季刚 <jigang.yf@alibaba-inc.com>
2019-05-02 17:08:45 +0800

    Kafka API

---
+++ src/main/java/com/ifan/kafka/KafkaClientApp.java
@@ -0,0 +1,11 @@
+package com.ifan.kafka;
+
+/**
+ * Kafka测试类
+ */
+
+public class KafkaClientApp {
+    public static void main(String[] args) {
+        new KafkaProducer(KafkaProperties.TOPIC).start();
+    }
+}
---
+++ src/main/java/com/ifan/kafka/KafkaProperties.java
@@ -0,0 +1,16 @@
+package com.ifan.kafka;
+
+/**
+ * Kafka常用配置文件
+ */
+
+public class KafkaProperties {
+
+    public static final String ZK = "localhost:2181";
+
+    public static final String TOPIC = "hello_topic";
+
+    public static final String BROKER_LIST = "localhost:9092";
+
+
+}
